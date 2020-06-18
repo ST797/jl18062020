8c6325c459f7c2942a2c7bee83504c42ebbcdf7c
Sushmith Thuluva <42782828+ST797@users.noreply.github.com>
2020-06-18 18:55:57 +0530

    Add files via upload

---
+++ weapons.js
@@ -0,0 +1,43 @@
+document.getElementById("hv").addEventListener("click",heat);
+function heat(){
+  document.getElementById("i").src = "heat vision.jpg";
+}
+
+document.getElementById("fb").addEventListener("click",freeze);
+function freeze(){
+  document.getElementById("i").src = "freezing breath.jpg";
+}
+
+document.getElementById("sp").addEventListener("click",punch);
+function punch(){
+  document.getElementById("i").src = "punch.jpg";
+}
+
+
+document.getElementById("ls").addEventListener("click",lasso);
+function lasso(){
+  document.getElementById("i").src = "ls.jpg";
+}
+
+document.getElementById("Bs").addEventListener("click",bracelet);
+function bracelet(){
+  document.getElementById("i").src = "Bracelet.jpg";
+}
+
+document.getElementById("SA").addEventListener("click",sword);
+function sword(){
+  document.getElementById("i").src = "sword.png";
+}
+
+document.getElementById("spunch").addEventListener("click",speedpunch);
+function speedpunch(){
+  document.getElementById("i").src = "spunch.jpg";
+}
+document.getElementById("smirage").addEventListener("click",speedmirage);
+function speedmirage(){
+  document.getElementById("i").src = "The Flash1.jpg";
+}
+document.getElementById("lbolt").addEventListener("click",lightning);
+function lightning(){
+  document.getElementById("i").src = "lbolt.jpg";
+}
